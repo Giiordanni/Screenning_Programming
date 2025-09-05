265ac5839902a0cbb7b136a53797778d811582a5
Giordanni <giordanniformiga103@gmail.com>
2025-09-05 16:09:25 -0300

    me conectando ao neon bd

--- db/bd_postgres.py
+++ db/bd_postgres.py
@@ -7,18 +7,25 @@ load_dotenv()
 
 def db_connection():
     try:
-        connection = psycopg2.connect(
-            host = os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            database=os.getenv('DB_DATABASE'),
-            # ssl_ca=os.getenv('DB_SSL_CA'),
-        )
-        print("Conectado com suceso ao banco de dados")
+        database_url = os.getenv('DATABASE_URL')
+        
+        if database_url:
+            connection = psycopg2.connect(database_url)
+        else:
+            connection = psycopg2.connect(
+                host=os.getenv('DB_HOST'),
+                port=os.getenv('DB_PORT'),
+                user=os.getenv('DB_USER'),
+                password=os.getenv('DB_PASSWORD'),
+                database=os.getenv('DB_DATABASE'),
+                sslmode=os.getenv('DB_SSL_MODE'),
+                channel_binding=os.getenv('DB_CHANNEL_BINDING')
+            )
+        
+        print("Conectado com sucesso ao banco de dados")
         return connection
     except Error as e:
-            print(f"Error: {e}")
-            return None
+        print(f"Erro na conexão com o banco: {e}")
+        return None
     
     
